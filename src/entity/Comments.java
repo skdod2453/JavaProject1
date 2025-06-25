5bec22d101607d064934cda4c5e2fe1b6c60e832
skdod2453 <skdod2453@gmail.com>
2025-06-25 13:14:36 +0900

    feat: 댓글 엔티티 Comments 클래스 생성

---
+++ src/entity/Comments.java
@@ -0,0 +1,36 @@
+package entity;
+
+import java.util.Date;
+
+public class Comments {
+    private int id;
+    private int postId;
+    private int userId;
+    private String content;
+    private Date createdAt;
+
+    public Comments() {
+    }
+
+    public Comments(int id, int postId, int userId, String content, Date createdAt) {
+        this.id = id;
+        this.postId = postId;
+        this.userId = userId;
+        this.content = content;
+        this.createdAt = createdAt;
+    }
+
+    // get
+    public int getId() { return id; }
+    public int getPostId() { return postId; }
+    public int getUserId() { return userId; }
+    public String getContent() { return content; }
+    public Date getCreatedAt() { return createdAt; }
+
+    // set
+    public void setId(int id) { this.id = id; }
+    public void setPostId(int postId) { this.postId = postId; }
+    public void setUserId(int userId) { this.userId = userId; }
+    public void setContent(String content) { this.content = content; }
+    public void setCreatedAt(Date createdAt) { this.createdAt = createdAt;}
+}
