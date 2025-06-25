19ddf2acba004c45ea26026a0c2a0ed33bc79d5c
skdod2453 <skdod2453@gmail.com>
2025-06-25 13:45:41 +0900

    feat: PostDao 인터페이스 생성 (글 정보 CRUD 기능)

--- src/dao/UserDao.java
+++ src/dao/UserDao.java
@@ -5,9 +5,9 @@ import entity.Users;
 import java.util.List;
 
 public interface UserDao {
-    void insertUser(Users users);
-    Users getUserById(int id);
-    List<Users> getAllUsers();
-    void updateUser(Users users);
-    void deleteUser(int id);
+    void insertUser(Users users);       // 회원 추가
+    Users getUserById(int id);          // id로 회원 조회
+    List<Users> getAllUsers();          // 모든 회원 조회
+    void updateUser(Users users);       // 회원 수정
+    void deleteUser(int id);            // 회원 삭제
 }
