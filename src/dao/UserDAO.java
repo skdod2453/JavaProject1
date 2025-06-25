5886a4a7169808e551ee22f788420a4a0b070175
skdod2453 <skdod2453@gmail.com>
2025-06-25 16:29:42 +0900

    feat: UserDAO 수정 및 UserDAOImpl 회원 추가,유저 닉네임 가져오기 추가

--- src/dao/UserDAO.java
+++ src/dao/UserDAO.java
@@ -5,8 +5,5 @@ import java.util.List;
 
 public interface UserDAO {
     void insertUser(User users);       // 회원 추가
-    User getUserById(int id);          // id로 회원 조회
-    List<User> getAllUsers();          // 모든 회원 조회
-    void updateUser(User users);       // 회원 수정
-    void deleteUser(int id);            // 회원 삭제
+    User getUserByNickname(String nickname);
 }
--- src/dao/UserDAOImpl.java
+++ src/dao/UserDAOImpl.java
@@ -1,5 +1,44 @@
 package dao;
 
+import entity.User;
+import java.sql.*;
+
 public class UserDAOImpl implements UserDAO {
+    private final Connection conn;
+
+    public UserDAOImpl(Connection conn) {
+        this.conn = conn;
+    }
+
+    @Override
+    public void insertUser(User user) {
+        String sql = "INSERT INTO users (nickname) VALUES (?)";
+        try (PreparedStatement pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            pstmt.setString(1, user.getNickname());
+            pstmt.executeUpdate();
+            ResultSet rs = pstmt.getGeneratedKeys();
+            if (rs.next()) user.setId(rs.getInt(1));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public User getUserByNickname(String nickname) {
+        String sql = "SELECT * FROM users WHERE nickname = ?";
+        try (PreparedStatement pstmt = conn.prepareStatement(sql)) {
+            pstmt.setString(1, nickname);
+            ResultSet rs = pstmt.executeQuery();
+            if (rs.next()) {
+                return new User(
+                        rs.getInt("id"),
+                        rs.getString("nickname")
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
 }
