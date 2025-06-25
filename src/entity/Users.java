6ca3adeb2cec96e2205351855d9d35bbf7c70c4f
skdod2453 <skdod2453@gmail.com>
2025-06-25 11:31:31 +0900

    feat: Users 엔티티 클래스 추가

---
+++ src/entity/Users.java
@@ -0,0 +1,29 @@
+package entity;
+
+public class Users {
+    private int id;
+    private String nickname;
+
+    public Users() {}
+
+    public Users(int id, String nickname) {
+        this.id = id;
+        this.nickname = nickname;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+}
