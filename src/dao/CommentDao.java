3a6255c5a5c0f9719735c268155e978fe5244f2a
skdod2453 <skdod2453@gmail.com>
2025-06-25 13:49:27 +0900

    feat: CommentDao 인터페이스 생성 (댓글 정보 CRUD 기능)

---
+++ src/dao/CommentDao.java
@@ -0,0 +1,13 @@
+package dao;
+
+import entity.Comments;
+
+import java.util.List;
+
+public interface CommentDao {
+    void insertCommentDao(Comments comments);   // 댓글 작성
+    Comments getCommentById(int id);            // id로 댓글 조회
+    List<Comments> getAllComments();            // 모든 댓글 조회
+    void updateComments(Comments comments);     // 댓글 수정
+    void deleteComments(int id);                // 댓글 삭제
+}
